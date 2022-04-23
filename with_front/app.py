5b803725b55bdd92a49e0f7c81a77e6d31cacade
Armi <arman@devjs.ru>
2022-04-23 23:20:11 +0300

    schedule

--- with_front/app.py
+++ with_front/app.py
@@ -2,7 +2,7 @@ import requests
 import schedule
 
 
-url = "https://auto.ru/moskva/cars/used/?year_from=2012&price_to=1299999&catalog_filter=mark%3DINFINITI&catalog_filter=mark%3DSUZUKI&catalog_filter=mark%3DLAND_ROVER&catalog_filter=mark%3DVOLKSWAGEN&catalog_filter=mark%3DCHEVROLET&catalog_filter=mark%3DCITROEN&catalog_filter=mark%3DPORSCHE&catalog_filter=mark%3DSKODA&catalog_filter=mark%3DOPEL&catalog_filter=mark%3DMITSUBISHI&catalog_filter=mark%3DMAZDA&catalog_filter=mark%3DLEXUS&catalog_filter=mark%3DMINI&catalog_filter=mark%3DRENAULT&catalog_filter=mark%3DTOYOTA&catalog_filter=mark%3DFORD&catalog_filter=mark%3DHYUNDAI&catalog_filter=mark%3DBMW&catalog_filter=mark%3DNISSAN&catalog_filter=mark%3DKIA&catalog_filter=mark%3DVOLVO&catalog_filter=mark%3DHONDA&catalog_filter=mark%3DAUDI&catalog_filter=mark%3DMERCEDES&seller_group=PRIVATE&sort=price-asc&top_days=3&page=1"
+url = "https://auto.ru/moskva/cars/used/?year_from=2012&price_to=1299999&catalog_filter=mark%3DINFINITI&catalog_filter=mark%3DSUZUKI&catalog_filter=mark%3DLAND_ROVER&catalog_filter=mark%3DVOLKSWAGEN&catalog_filter=mark%3DCHEVROLET&catalog_filter=mark%3DCITROEN&catalog_filter=mark%3DPORSCHE&catalog_filter=mark%3DSKODA&catalog_filter=mark%3DOPEL&catalog_filter=mark%3DMITSUBISHI&catalog_filter=mark%3DMAZDA&catalog_filter=mark%3DLEXUS&catalog_filter=mark%3DMINI&catalog_filter=mark%3DRENAULT&catalog_filter=mark%3DTOYOTA&catalog_filter=mark%3DFORD&catalog_filter=mark%3DHYUNDAI&catalog_filter=mark%3DBMW&catalog_filter=mark%3DNISSAN&catalog_filter=mark%3DKIA&catalog_filter=mark%3DVOLVO&catalog_filter=mark%3DHONDA&catalog_filter=mark%3DAUDI&catalog_filter=mark%3DMERCEDES&seller_group=PRIVATE&sort=cr_date-desc&top_days=3&page=1"
 
 
 def job():
